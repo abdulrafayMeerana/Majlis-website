2f326adcf66532b75af02d5868c148e787b72b90
Abdulla <abdulla.hameed@meerana.ae>
2023-12-14 13:24:46 +0530

    section 4 completed

--- src/components/SectionFour.js
+++ src/components/SectionFour.js
@@ -1,77 +1,169 @@
 const SectionFour = () => {
   return (
-    <section className="self-stretch flex flex-col items-center justify-center gap-[32px] min-h-[272px] text-center text-5xl text-burlywood-200 font-cairo">
-      <h1 className="m-0 relative text-inherit leading-[40px] font-semibold font-inherit inline-block w-64 h-10 shrink-0">
-        الدستور واللوائح والانظمة
-      </h1>
-      <div className="self-stretch flex flex-row flex-wrap items-center justify-start py-0 px-[53px] gap-[82px] text-base text-white">
-        <div className="relative rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply">
-          <img
-            className="absolute top-[-80px] left-[-53px] rounded-lg w-[250px] h-[250px] object-cover mix-blend-multiply"
-            alt=""
-            src="/image-87@2x.png"
-          />
-          <div className="absolute top-[12px] left-[16px] leading-[28px] font-semibold ">
-            <p className="m-0">نظام لجان</p>
-            <p className="m-0"> المجلس الوطني</p>
+    <>
+      <section className="self-stretch bg-blak flex flex-col items-center justify-center text-center text-5xl text-burlywood-200 font-cairo">
+        <h1 className="m-0  relative text-inherit leading-[40px] font-semibold font-inherit inline-block w-64 h-10 shrink-0">
+          الدستور واللوائح والانظمة
+        </h1>
+        {/* <div className="self-stretch grid grid-cols-6 lg:grid-cols-3 px-10 bg-blac flex-row flex-wrap items-center justify-around py-0 text-base text-white">
+          <div className="relative rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply">
+            <img
+              className="absolute top-[-80px] left-[-53px] rounded-lg w-[250px] h-[250px] object-cover mix-blend-multiply"
+              alt=""
+              src="/image-87@2x.png"
+            />
+            <div className="absolute top-[12px] left-[16px] leading-[28px] font-semibold ">
+              <p className="m-0">نظام لجان</p>
+              <p className="m-0"> المجلس الوطني</p>
+            </div>
           </div>
-        </div>
-        <div className="relative rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply">
-          <img
-            className="absolute top-[-80px] left-[-53px] rounded-lg w-[250px] h-[250px] object-cover  mix-blend-multiply"
-            alt=""
-            src="/image-87@2x.png"
-          />
-          <div className="absolute top-[12px] left-[29px] leading-[28px] font-semibold">
-            <p className="m-0">نظام عمل</p>
-            <p className="m-0">رؤساء اللجان</p>
+          <div className="relative rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply">
+            <img
+              className="absolute top-[-80px] left-[-53px] rounded-lg w-[250px] h-[250px] object-cover  mix-blend-multiply"
+              alt=""
+              src="/image-87@2x.png"
+            />
+            <div className="absolute top-[12px] left-[29px] leading-[28px] font-semibold">
+              <p className="m-0">نظام عمل</p>
+              <p className="m-0">رؤساء اللجان</p>
+            </div>
           </div>
-        </div>
-        <div className="relative rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply">
-          <img
-            className="absolute top-[-80px] left-[-53px] rounded-lg w-[250px] h-[250px] object-cover  mix-blend-multiply"
-            alt=""
-            src="/image-87@2x.png"
-          />
-          <div className="absolute top-[12px] left-[27px] leading-[28px] font-semibold">
-            <p className="m-0">نظام عمل</p>
-            <p className="m-0"> هيئة المكتب</p>
+          <div className="relative rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply">
+            <img
+              className="absolute top-[-80px] left-[-53px] rounded-lg w-[250px] h-[250px] object-cover  mix-blend-multiply"
+              alt=""
+              src="/image-87@2x.png"
+            />
+            <div className="absolute top-[12px] left-[27px] leading-[28px] font-semibold">
+              <p className="m-0">نظام عمل</p>
+              <p className="m-0"> هيئة المكتب</p>
+            </div>
           </div>
-        </div>
-        <div className="relative rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply">
-          <div className="absolute top-[12px] left-[16px] leading-[28px] font-semibold">
-            <p className="m-0">النظام الداخلي</p>
-            <p className="m-0">للشعبة البرلمانية</p>
+          <div className="relative rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply">
+            <div className="absolute top-[12px] left-[16px] leading-[28px] font-semibold">
+              <p className="m-0">النظام الداخلي</p>
+              <p className="m-0">للشعبة البرلمانية</p>
+            </div>
+            <img
+              className="absolute top-[-80px] left-[-53px] rounded-lg w-[250px] h-[250px] object-cover  mix-blend-multiply"
+              alt=""
+              src="/image-87@2x.png"
+            />
           </div>
-          <img
-            className="absolute top-[-80px] left-[-53px] rounded-lg w-[250px] h-[250px] object-cover  mix-blend-multiply"
-            alt=""
-            src="/image-87@2x.png"
-          />
-        </div>
-        <div className="relative rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply">
-          <div className="absolute top-[21px] left-[23px] leading-[40px] font-semibold">
-            اللائحة الداخلية
+          <div className="relative rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply">
+            <div className="absolute top-[21px] left-[23px] leading-[40px] font-semibold">
+              اللائحة الداخلية
+            </div>
+            <img
+              className="absolute top-[-80px] left-[-53px] rounded-lg w-[250px] h-[250px] object-cover mix-blend-multiply"
+              alt=""
+              src="/image-87@2x.png"
+            />
           </div>
+          <div className="relative w-[200px] h-[200px]">
+            <img
+              className="absolute top-[-20px] left-[-25px] rounded-lg w-[250px] h-[250px] object-cover"
+              alt=""
+              src="/image-92@2x.png"
+            />
+            <div className="absolute top-[60px] left-[28px] rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply" />
+            <div className="absolute top-[81px] left-[74px] leading-[40px] font-semibold">
+              الدستور
+            </div>
+          </div>
+        </div> */}
+      </section>
+      
+      <div className="font-cairo bg-red00 text-white grid xl:grid-cols-6 md:grid-cols-3 lg:grid-cols-4 sm:grid-cos-1 ">
+        {/* 1 */}
+        <div className="relative">
           <img
-            className="absolute top-[-80px] left-[-53px] rounded-lg w-[250px] h-[250px] object-cover mix-blend-multiply"
-            alt=""
-            src="/image-87@2x.png"
-          />
-        </div>
-        <div className="relative w-[200px] h-[200px]">
-          <img
-            className="absolute top-[-20px] left-[-25px] rounded-lg w-[250px] h-[250px] object-cover"
+            className="relative rounded-lg border-2 w-[250px] h-[250px] opacity-100"
             alt=""
             src="/image-92@2x.png"
           />
-          <div className="absolute top-[60px] left-[28px] rounded bg-burlywood-200 w-36 h-[81px] mix-blend-multiply" />
-          <div className="absolute top-[81px] left-[74px] leading-[40px] font-semibold">
+          <div className="absolute top-20 left-[52px] grid h-[80px] w-[140px] rounded-[8px] bg-burlywood-200 leading-[28px] font-semibold ">
+            <div className="justify-self-center  place-self-center text-center">
+              <p className="m-0">نظام لجان</p>
+              <p className="m-0"> المجلس الوطني</p>
+            </div>
+          </div>
+        </div>
+        {/* 2  */}
+
+        <div className="relative">
+          <img
+            className="relative rounded-lg border-2 w-[250px] h-[250px] opacity-100"
+            alt=""
+            src="/image-92@2x.png"
+          />
+          <div className="absolute top-20 left-[52px] grid h-[80px] w-[140px] rounded-[8px] bg-burlywood-200 leading-[28px] font-semibold ">
+            <div className="justify-self-center  place-self-center text-center">
+              <p className="m-0">نظام عمل</p>
+              <p className="m-0">رؤساء اللجان</p>
+            </div>
+          </div>
+        </div>
+        {/* 3 */}
+
+        <div className="relative">
+          <img
+            className="relative rounded-lg border-2 w-[250px] h-[250px] opacity-100"
+            alt=""
+            src="/image-92@2x.png"
+          />
+          <div className="absolute top-20 left-[52px] grid h-[80px] w-[140px] rounded-[8px] bg-burlywood-200 leading-[28px] font-semibold ">
+            <div className="justify-self-center  place-self-center text-center">
+              <p className="m-0">نظام عمل</p>
+              <p className="m-0"> هيئة المكتب</p>
+            </div>
+          </div>
+        </div>
+        {/* 4 */}
+        <div className="relative">
+          <img
+            className="relative rounded-lg border-2 w-[250px] h-[250px] opacity-100"
+            alt=""
+            src="/image-92@2x.png"
+          />
+          <div className="absolute top-20 left-[52px] grid h-[80px] w-[140px] rounded-[8px] bg-burlywood-200 leading-[28px] font-semibold ">
+            <div className="justify-self-center  place-self-center text-center">
+              <p className="m-0">النظام الداخلي</p>
+              <p className="m-0">للشعبة البرلمانية</p>
+            </div>
+          </div>
+        </div>
+
+        {/* 5 */}
+        <div className="relative">
+          <img
+            className="relative rounded-lg border-2 w-[250px] h-[250px] opacity-100"
+            alt=""
+            src="/image-92@2x.png"
+          />
+          <div className="absolute top-20 left-[52px] grid h-[80px] w-[140px] rounded-[8px] bg-burlywood-200 leading-[28px] font-semibold ">
+            <div className="justify-self-center  place-self-center text-center">
+              <p className="m-0">اللائحة الداخلية</p>
+            </div>
+          </div>
+        </div>
+        {/* 6 */}
+
+        <div className="relative">
+          <img
+            className="relative rounded-lg border-2 w-[250px] h-[250px] opacity-100"
+            alt=""
+            src="/image-92@2x.png"
+          />
+          <div className="absolute top-20 left-[52px] grid h-[80px] w-[140px] rounded-[8px] bg-burlywood-200 leading-[28px] font-semibold ">
+            <div className="justify-self-center  place-self-center text-center">
             الدستور
           </div>
         </div>
+        </div>
+
       </div>
-    </section>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -56,13 +56,21 @@ module.exports = {
       inherit: "inherit",
     },
     screens: {
-      lg: {
-        max: "1200px",
-      },
-      md: {
-        max: "960px",
-      },
-    },
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   corePlugins: {
     preflight: false,
